6cc9fa1973b8d1396202d69825c0c8b157ebc888
runminty <zengzhenhuang@gmail.com>
2019-04-04 15:43:05 +1300

    Fix CSS. Define consts.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+const feet = 0.3048;
+const miles = 1.609;
+const pounds = 2.205;
+const tons = 2204.6;
+const fahrenheit = 1.8;
+const gallons = 4.54609;
+const cubicinches = 16.387;
 
+function convertToMetric () {
+  
+}
