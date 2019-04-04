f1a2f55411c47183ebfb25f16841dec509c0f215
Matthew Sew Hoy <mktsewhoy@hotmail.co.nz>
2019-04-05 01:44:01 +1300

    Both fields calculate on the fly.

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const feet = 0.3048;
-const miles = 1.609;
+const miles = 1.60934;
 const pounds = 2.205;
 const tons = 2204.6;
 const fahrenheit = 1.8;
 const gallons = 4.54609;
 const cubicinches = 16.387;
 
-function convertToImperial (n) {
-//     var from = document.getElementById("from").innerHTML;
-//     var to = document.getElementById("to").innerHTML;
-//   to = from * miles;
-document.getElementById("to").innerHTML = n * miles;
+function convertTo (source, n) {
+    n = parseFloat(n);
+    // var inp = document.getElementById("fromUnit").innerHTML;
+    // var out = document.getElementById("toUnit").innerHTML;
+    var inp = document.getElementById("fromUnit");
+    var out = document.getElementById("toUnit");
+
+    if (source == "fromUnit") {
+        out.value = (n * miles).toFixed(3);
+    }
+    if (source == "toUnit") {
+        inp.value = (n / miles).toFixed(3);
+    }    
 }
